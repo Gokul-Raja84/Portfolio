1ee3e12b85bdb22943b66f7cb5828b8ab74cb98b
Gokul-Raja84 <gokulraja840@gmail.com>
2024-07-22 11:59:35 +0530

    portfolio changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className={''}>
+    <Html lang="en" className={""}>
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-3923445118031594"
+      ></meta>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
